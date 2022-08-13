e6b6872c6783216dfc8c7d81fed77aeab1ebb272
Yura Filatov <yura.fila@gmail.com>
2022-08-13 11:16:24 -0400

    Initial Commit

---
+++ RecycleUI/RecycleUI.h
@@ -0,0 +1,18 @@
+//
+//  RecycleUI.h
+//  RecycleUI
+//
+//  Created by Yura on 8/13/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RecycleUI.
+FOUNDATION_EXPORT double RecycleUIVersionNumber;
+
+//! Project version string for RecycleUI.
+FOUNDATION_EXPORT const unsigned char RecycleUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RecycleUI/PublicHeader.h>
+
+
