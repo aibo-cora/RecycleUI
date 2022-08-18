b3a1af935e8a0c1f6bd386217dfb4c55563836d3
Yura Filatov <yura.fila@gmail.com>
2022-08-18 10:25:17 -0400

    Composable carousel.

--- RecycleUI/Views/Carousel/Carousel.swift
+++ RecycleUI/Views/Carousel/Carousel.swift
@@ -7,8 +7,8 @@
 
 import SwiftUI
 
-struct Carousel<Profile>: View where Profile: EntityProfile {
-    @State private var centeredTileIndex = -1/// keep track on the visible tile by using a drag gesture and updating the index.
+struct Carousel<Profile, Footer>: View where Profile: EntityProfile, Footer: View {
+    @State private var centeredTileIndex = 0/// keep track on the visible tile by using scroll view offset and updating the index.
     @State private var offset: CGSize = .zero
     
     var data: [Profile]
@@ -18,63 +18,66 @@ struct Carousel<Profile>: View where Profile: EntityProfile {
     let space = "scrollview"
     let tileSpacing: CGFloat = 10
     
+    var centerIndexChanged: ((_ index: Int) -> Void)?
+    var footer: ((_ index: Int) -> Footer)?
+    
+    @ViewBuilder
     var body: some View {
-        ScrollView(.horizontal, showsIndicators: false) {
-            HStack(spacing: tileSpacing) {
-                ForEach(Array(data.enumerated()), id: \.element) { index, profile in
-                    CarouselTile(centeredTileIndex: $centeredTileIndex, profile: profile, tileCornerRadius: tileCornerRadius, tileSize: tileSize)
-                        .scaleEffect(index == centeredTileIndex ? 1.0 : 1.0)
-                        .zIndex(Double(index == centeredTileIndex ? 100 : index))
-                        .overlay {
-                            RoundedRectangle(cornerRadius: tileCornerRadius, style: .circular)
-                                .frame(width: tileSize.width, height: tileSize.height)
-                                .foregroundColor(index == centeredTileIndex ? .clear : .black)
-                                .opacity(index == centeredTileIndex ? 1 : 0.5)
-                        }
-
-                        // .offset(x: (UIScreen.main.bounds.width - tileSize.width) / 2, y: 0)
-                        .animation(SwiftUI.Animation.easeInOut(duration: 0.5).delay(0.25), value: centeredTileIndex)
-                }
-            }
-            .padding([.leading, .trailing], 20)
-            .background {
-                GeometryReader { geometry in
-                    Color.clear.preference(
-                        key: ScrollOffsetPreferenceKey.self,
-                        value: geometry.frame(in: .named(space)).origin.x
-                    )
-                }
-                // .frame(width: 0, height: 0, alignment: .center) <- Bad things happen
-                .onPreferenceChange(ScrollOffsetPreferenceKey.self) { svOffset in
-                    let totalTileWidth = tileSize.width + tileSpacing
-                    let totalContentWidth = CGFloat(data.count) * CGFloat(tileSize.width + tileSpacing)
-                    let hack = svOffset - 100
-                    
-                    print(svOffset, hack, totalContentWidth, totalTileWidth)
-                    
-                    if svOffset > -(totalContentWidth - totalTileWidth - 150) {
-                        centeredTileIndex = abs(Int(hack / totalTileWidth))
-                    } else {
-                        centeredTileIndex = data.count - 1
+        VStack {
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: tileSpacing) {
+                    ForEach(Array(data.enumerated()), id: \.element) { index, profile in
+                        CarouselTile(centeredTileIndex: $centeredTileIndex, profile: profile, tileCornerRadius: tileCornerRadius, tileSize: tileSize, index: index)
+                            .overlay {
+                                RoundedRectangle(cornerRadius: tileCornerRadius, style: .circular)
+                                    .frame(width: tileSize.width, height: tileSize.height)
+                                    .foregroundColor(index == centeredTileIndex ? .clear : .black)
+                                    .opacity(index == centeredTileIndex ? 1 : 0.5)
+                            }
                     }
-                    
+                }
+                .padding([.leading, .trailing], 20)
+                .background {
+                    GeometryReader { geometry in
+                        Color.clear.preference(
+                            key: ScrollOffsetPreferenceKey.self,
+                            value: geometry.frame(in: .named(space)).origin.x
+                        )
+                    }
+                    // .frame(width: 0, height: 0, alignment: .center) <- Bad things happen
+                    .onPreferenceChange(ScrollOffsetPreferenceKey.self) { svOffset in
+                        let totalTileWidth = tileSize.width + tileSpacing
+                        let totalContentWidth = CGFloat(data.count) * CGFloat(tileSize.width + tileSpacing)
+                        let hack = svOffset - 100
+                        
+                        print(svOffset, hack, totalContentWidth, totalTileWidth)
+                        
+                        if svOffset > -(totalContentWidth - totalTileWidth - 150) {
+                            centeredTileIndex = abs(Int(hack / totalTileWidth))
+                        } else {
+                            centeredTileIndex = data.count - 1
+                        }
+                    }
                 }
             }
+            .coordinateSpace(name: space)
+            
+            footer?(centeredTileIndex)
         }
-        .coordinateSpace(name: space)
+        .animation(SwiftUI.Animation.linear(duration: 0.5).delay(0.25), value: centeredTileIndex)
     }
     
     struct CarouselTile: View {
         @Binding var centeredTileIndex: Int
         
-        var profile: Profile
-        
+        let profile: Profile
         var defaultProfilePhotoLocation = ""
-        var tileCornerRadius: CGFloat
-        var tileSize: CGSize
+        let tileCornerRadius: CGFloat
+        let tileSize: CGSize
+        let index: Int
         
         var body: some View {
-            ZStack() {
+            VStack {
                 AsyncImage(url: URL(string: profile.profilePhotoLocation ?? defaultProfilePhotoLocation)) { image in
                     image
                         .resizable()
@@ -84,15 +87,6 @@ struct Carousel<Profile>: View where Profile: EntityProfile {
                 } placeholder: {
                     ProgressView()
                 }
-//                .overlay(alignment: .bottom) {
-//                    RoundedRectangle(cornerRadius: tileCornerRadius, style: .circular)
-//                        .frame(width: tileSize.width, height: tileSize.height / 3.5)
-//                        .foregroundColor(.white)
-//                    Text(profile.profileInfo ?? "I am still considering what to put here.")
-//                        .padding()
-//                        .lineLimit(3)
-//                        .minimumScaleFactor(0.5)
-//                }
             }
         }
     }
@@ -101,13 +95,17 @@ struct Carousel<Profile>: View where Profile: EntityProfile {
 struct Carousel_Previews: PreviewProvider {
     static var previews: some View {
         Group {
-            Carousel(data: UserStore().profiles)
-                .previewDevice(PreviewDevice(rawValue: "iPhone 13 Pro Max"))
-                .previewDisplayName("iPhone 13 Pro Max")
+            Carousel(data: UserStore().profiles, footer:  { index in
+                Text("")
+            })
+            .previewDevice(PreviewDevice(rawValue: "iPhone 13 Pro Max"))
+            .previewDisplayName("iPhone 13 Pro Max")
             
-            Carousel(data: UserStore().profiles)
-                .previewDevice(PreviewDevice(rawValue: "iPad Pro (11-inch) (3rd generation)"))
-                .previewDisplayName("iPad Pro (11-inch) (3rd generation)")
+            Carousel(data: UserStore().profiles, footer:  { index in
+                Text("")
+            })
+            .previewDevice(PreviewDevice(rawValue: "iPad Pro (11-inch) (3rd generation)"))
+            .previewDisplayName("iPad Pro (11-inch) (3rd generation)")
         }
     }
 }
--- RecycleUI/Views/Carousel/Examples/CarouselExample.swift
+++ RecycleUI/Views/Carousel/Examples/CarouselExample.swift
@@ -9,12 +9,27 @@ import SwiftUI
 
 struct CarouselExample: View {
     var body: some View {
-        Carousel(data: UserStore().profiles)
+        Carousel(data: UserStore().profiles, footer: { index in
+//            Text(UserStore().profiles[index].profileInfo ?? "I am still considering what to put here.")
+//                .lineLimit(3)
+//                .minimumScaleFactor(0.5)
+//                .padding([.top], 50)
+//                .padding([.leading, .trailing], 20)
+            EmptyView()
+        })
     }
 }
 
 struct CarouselExample_Previews: PreviewProvider {
     static var previews: some View {
-        CarouselExample()
+        Group {
+            CarouselExample()
+                .previewDevice(PreviewDevice(rawValue: "iPhone 13 Pro Max"))
+                .previewDisplayName("iPhone 13 Pro Max")
+            
+            CarouselExample()
+                .previewDevice(PreviewDevice(rawValue: "iPad Pro (11-inch) (3rd generation)"))
+                .previewDisplayName("iPad Pro (11-inch) (3rd generation)")
+        }
     }
 }
